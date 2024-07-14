5b446556f32f481090073a7961d23274b6fec6eb
ducmanh2503 <traubudz@gmail.com>
2024-07-14 22:18:31 +0700

    update admin

---
+++ admin/danhmuc/list.php
@@ -0,0 +1,87 @@
+<!-- Page wrapper  -->
+<!-- ============================================================== -->
+<div class="page-wrapper">
+  <!-- ============================================================== -->
+  <!-- Bread crumb and right sidebar toggle -->
+  <!-- ============================================================== -->
+  <div class="page-breadcrumb">
+    <div class="row">
+      <div class="col-12 d-flex no-block align-items-center">
+        <h4 class="page-title">Danh mục sản phẩm</h4>
+        <div class="ml-auto text-right">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="#">Admin</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Danh mục</li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ============================================================== -->
+  <!-- End Bread crumb and right sidebar toggle -->
+  <!-- ============================================================== -->
+  <!-- ============================================================== -->
+  <!-- Container fluid  -->
+  <!-- ============================================================== -->
+  <div class="container-fluid">
+    <!-- ============================================================== -->
+    <!-- Start Page Content -->
+    <!-- ============================================================== -->
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Mã danh mục</th>
+                <th scope="col">Tên danh mục</th>
+                <th scope="col">Trạng thái</th>
+                <th scope="col">Tương tác</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td><a href=""><input type="button" value="Thay đổi"></a></td>
+                <td><a href="index.php?act=update_danhmuc"><input type="button" value="Sửa"></a></td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td><a href=""><input type="button" value="Thay đổi"></a></td>
+                <td><a href="index.php?act=update_danhmuc"><input type="button" value="Sửa"></a></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td><a href=""><input type="button" value="Thay đổi"></a></td>
+                <td><a href="index.php?act=update_danhmuc"><input type="button" value="Sửa"></a></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div style="display: flex; justify-content: center; align-items: center; ">
+          <a href="index.php?act=add_danhmuc" style="text-decoration: none;">
+            <input type="button" value="Thêm mới"
+              style="padding: 10px 20px; background-color: #007bff; color: #fff; border: none; cursor: pointer; font-size: 16px; border-radius: 5px;">
+          </a>
+        </div>
+
+      </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- End PAge Content -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Right sidebar -->
+    <!-- ============================================================== -->
+    <!-- .right-sidebar -->
+    <!-- ============================================================== -->
+    <!-- End Right sidebar -->
+    <!-- ============================================================== -->
+  </div>
